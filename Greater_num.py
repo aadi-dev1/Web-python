58d1b796d3b175126267672a870d01af4f65a949
aadi-dev1 <adarshsharma8219@gmail.com>
2026-04-18 08:40:11 +0530

    git config --global user.name "aadi-dev1" git config --global user.email "adarshsharma8219@gmail.com"

--- Greater_num.py
+++ Greater_num.py
@@ -1,2 +1,6 @@
 num1=int(input("Enter a Value:"))
-# num2=int(input("Enter a Value:"))
+num2=int(input("Enter a Value:"))
+if(num1>num2):
+    print("First number is Greater:",num1)
+else:
+    print("Second number is Greater:",num2)
